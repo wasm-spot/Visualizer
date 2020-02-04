5e9cd9c6aa2c1dece1190c9e2f3e8a08dc45aa5a
tqiu8 <tammy.qiu@yahoo.com>
2020-02-04 17:28:56 -0500

    fix Visualizer.csproj in order to run sub projects

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Visualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            if (args.Length == 0)
+            {               
+                System.Console.WriteLine("Please enter the file path to an assembly dll.");
+            }
+
+            Console.WriteLine(args[0]);
+        }
+    }
+}
